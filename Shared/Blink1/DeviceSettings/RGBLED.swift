bf5682f3ca496f643558103ea6b4ae5b5ccab2a8
Carlyn Maw <carlynorama@gmail.com>
2021-07-29 15:30:36 -0700

    re-add (not currently used)

---
+++ Shared/Blink1/DeviceSettings/RGBLED.swift
@@ -0,0 +1,57 @@
+//
+//  RGBLED.swift
+//  ColorCrossPlatform
+//
+//  Created by Labtanza on 7/26/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct RGBLED:Equatable, Codable {
+//    var red: UInt8
+//    var green: UInt8
+//    var blue: UInt8
+//    
+//    mutating func setColor(to newColor:Color) {
+//        red = newColor.redByte
+//        green = newColor.greenByte
+//        blue = newColor.blueByte
+//    }
+//    
+//    mutating func turnOff() {
+//        red = 0
+//        green = 0
+//        blue = 0
+//    }
+//    
+//    var isOff:Bool {
+//        red == 0 && green == 0 && blue == 0
+//    }
+//    
+//    var isOn:Bool {
+//        !isOff
+//    }
+//    
+//    var color:Color {
+//        Color(red: red, green: green, blue: blue)
+//    }
+//    
+//}
+//
+//extension RGBLED {
+//    
+//    static var defaultColor = Color.springGreen
+//    
+//    init() {
+//        red = RGBLED.defaultColor.redByte
+//        green = RGBLED.defaultColor.greenByte
+//        blue = RGBLED.defaultColor.blueByte
+//    }
+//    
+//    init(from color:Color) {
+//        red = color.redByte
+//        green = color.greenByte
+//        blue = color.blueByte
+//    }
+}
