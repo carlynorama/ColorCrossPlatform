0e43deca9e81132ddd0a7fbf3dbdcfbe547a8d28
Carlyn Maw <carlynorama@gmail.com>
2021-07-29 15:20:09 -0700

    device only knows name, no settings

--- Shared/Blink1/DeviceSettings/Blink1Device.swift
+++ Shared/Blink1/DeviceSettings/Blink1Device.swift
@@ -10,12 +10,19 @@ import Foundation
 
 struct Blink1Device {
     let id:String
-    let firmwareVersion:String
-    let LED1:RGBLED
-    let LED2:RGBLED
+    let firmwareVersion:String?
+    var serialNumber:String?
+    var nickname:String?
+//    var LED1:RGBLED
+//    var LED2:RGBLED
+}
 
-    
-    //current color patterns
-    //firmware
-    //id number?
+extension Blink1Device {
+    static var exampleDevice:Blink1Device = Blink1Device(
+        id: UUID().uuidString,
+        firmwareVersion: "302",
+        serialNumber: "3374299d"//,
+        //LED1: RGBLED(),
+        //LED2: RGBLED()
+    )
 }
