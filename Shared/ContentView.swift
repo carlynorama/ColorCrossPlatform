ba26351dfb5320d6dedbb606d199a4ea9dda019d
Carlyn Maw <carlynorama@gmail.com>
2021-07-30 13:44:03 -0700

    push to device dialog in own file

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -31,52 +31,20 @@ struct ContentView: View {
         }
         .padding()
         .sheet(isPresented: $showSheet) {
-            UpdateMessage(settings: $settings).environmentObject(store)
+            PushUpdateDialogView(settings: $settings).environmentObject(store)
         }
     }
     
 
 }
 
-struct UpdateMessage:View {
-    @Environment(\.presentationMode) var presentationMode
-    
-    @EnvironmentObject var store:Blink1DeviceStore
-    @Binding var settings:Blink1UISettings
-    
-    var body: some View {
-        VStack {
-            Text(store.getMessage(settings))
-            CopyToClipBoardButton(textToCopy: store.getMessage(settings))
-            CopyButton(textToCopy: store.getMessage(settings)) {
-                Text("Test")
-            }.buttonStyle(ExampleStyle())
-            Button("Press to dismiss") {
-                presentationMode.wrappedValue.dismiss()
-            }.buttonStyle(ExampleStyle())
-            .font(.title)
-            .padding()
-        }
-    }
-    
-}
+
 
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-
-struct ExampleStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color.springGreen)
-            .foregroundColor(.white)
-            .clipShape(Capsule())
     }
 }
 
---
+++ Shared/PushUpdateDialogView.swift
@@ -0,0 +1,42 @@
+//
+//  PushUpdateDialogView.swift
+//  ColorCrossPlatform
+//
+//  Created by Labtanza on 7/30/21.
+//
+
+import SwiftUI
+
+struct PushUpdateDialogView: View {
+
+        @Environment(\.presentationMode) var presentationMode
+        
+        @EnvironmentObject var store:Blink1DeviceStore
+        @Binding var settings:Blink1UISettings
+        
+        var body: some View {
+            VStack {
+                Text(store.getMessage(settings))
+                CopyButton(textToCopy: store.getMessage(settings)) {
+                    Text("Test")
+                }
+                .font(.title)
+                .padding()
+                Button("Press to dismiss") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .font(.title)
+                .padding()
+            }
+        }
+        
+    
+}
+
+struct PushUpdateDialogView_Previews: PreviewProvider {
+    static let previewStore = Blink1DeviceStore()
+    
+    static var previews: some View {
+        PushUpdateDialogView(settings: .constant(Blink1UISettings.exampleSettings)).environmentObject(previewStore)
+    }
+}
